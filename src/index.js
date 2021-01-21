1a4a6ae9ee0487f48315f052714a99b2814532f8
dkm94 <diane.mpacko@gmail.com>
2021-01-21 01:58:57 +0100

    fix issue npx browserslist --update-db

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import '../src/App.css';
+import '../src/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
