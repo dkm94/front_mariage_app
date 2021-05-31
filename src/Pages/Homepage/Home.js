89216296a143eb38d8fcd9e80105345faf6c2828
dkm94 <diane.mpacko@gmail.com>
2021-05-31 17:44:20 +0200

    fix responsive /home et navbar

--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -7,13 +7,17 @@ const Home = () => {
 
         return (
             <div className="home">
+                <div className="home___position">
                     <div className="home-desc">
                         <h1>Votre mariage à portée de main</h1>
                         <span>Consultez et organisez facilement les étapes clés de votre mariage en quelques clics</span>
                     </div>
-                    <Link to={"/register"} className="home-link btn-style">
-                    Inscrivez-vous dès maintenant
-                    </Link>
+                    <div className="home___register-btn">
+                        <Link to={"/register"} className="home-link btn-style">
+                        Inscrivez-vous dès maintenant
+                        </Link>
+                    </div>
+                </div>
             </div>
         )
 }
