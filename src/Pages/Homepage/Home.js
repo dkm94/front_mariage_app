5106a9391f685b5754dd7db85d0491914a78a97c
dkm94 <diane.mpacko@gmail.com>
2021-10-27 19:48:00 +0200

    update homepage style

--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { withRouter, Link } from "react-router-dom";
+import flowers from "../../img/bouquet.png"
 import './Home.css';
 import "../../components/LargeButton/LargeButton.css"
 
-const Home = (props) => {
+const Home = () => {
         return (
             <div className="home">
                 <div className="home___position">
-                    <div className="home-desc">
-                        <h1>Votre mariage à portée de main</h1>
-                        <span>Consultez et organisez facilement les étapes clés de votre mariage en quelques clics</span>
+                    <div className="home___position-style">
+                        <div className="home-desc">
+                            <h1>Simplifiez l'organisation de votre mariage</h1>
+                            <p>
+                                <span>Consultez et gérez facilement les étapes clés de votre mariage en quelques clics.</span>
+                                <span>My Wedding, l'outil indispensable pour ne rien oublier des préparatifs de l'un des plus beaux évènements de votre vie.</span>
+                            </p>
+                        </div>
+                        <div className="home___register-btn">
+                            <Link to={"/register"} className="home-link btn-style">
+                            Inscrivez-vous dès maintenant
+                            </Link>
+                        </div>
                     </div>
-                    <div className="home___register-btn">
-                        <Link to={"/register"} className="home-link btn-style">
-                        Inscrivez-vous dès maintenant
-                        </Link>
-                    </div>
+                </div>
+                <div className="home__picture">
+                    <img src={flowers} alt="home page illustration flowers" />
                 </div>
             </div>
         )
--- src/components/Header/Navigation/Log_out.js
+++ src/components/Header/Navigation/Log_out.js
@@ -4,9 +4,11 @@
  import Logo from "../../../img/logo2.png"
  
 const Logout = () => {
-    
+
+    const path = window.location.pathname;
+
     return (
-    <div className="header header-style">
+    <div className="header header-style" style={path === "/" ? {backgroundColor: "#e8e8e4"} : {backgroundColor: "#FFF"}}>
         <div className="navigation">
             <div id="brand">
                 <Link to={{ pathname: "/"}}><img alt="logo" src={Logo} id="logo" /></Link>
