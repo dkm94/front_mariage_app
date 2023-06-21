2a383b7d772691da73671682c79044c359b6915f
dkm94 <diane.mpacko@gmail.com>
2023-06-21 16:37:15 +0200

    changed website layout UI

--- src/components/Header/Navigation/NavigationData.js
+++ src/components/Header/Navigation/NavigationData.js
@@ -20,28 +20,28 @@ export const NavigationData = [
         pathname: "/tableau-de-bord"
     },
     {
-        title: "invités",
+        title: "Invités",
         // icon: <PeopleAltIcon />,
         icon: guestsIcon,
         pathname: "/menu/invites"
     },
     {
-        title: "plan de table",
+        title: "Plan de table",
         icon: tablesIcon,
         pathname: "/menu/tables"
     },
     {
-        title: "tâches",
+        title: "Tâches",
         icon: todoIcon,
         pathname: "/menu/taches"
     },
     {
-        title: "réception",
+        title: "Réception",
         icon: menuIcon,
         pathname: "/menu/carte"
     },
     {
-        title: "dépenses",
+        title: "Dépenses",
         icon: expensesIcon,
         pathname: "/menu/budget"
     }
