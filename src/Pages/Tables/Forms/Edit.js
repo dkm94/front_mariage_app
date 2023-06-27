923841b162df8363620b698adffd8ba506152882
dkm94 <diane.mpacko@gmail.com>
2023-06-27 12:32:45 +0200

    fixed tables edit form

--- src/Pages/Tables/Forms/Edit.js
+++ src/Pages/Tables/Forms/Edit.js
@@ -1,62 +1,84 @@
 import React from "react";
 import Select from "../../../components/AsyncSelect/AsyncSelect";
-import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Button, IconButton } from "@mui/material";
 import "../Tables.css";
 import Table from "../Table";
+import BlackButton from "../../../components/Buttons/Black/BlackButton";
 
 const TableId = (props) => {
-    const { tables, 
-        table, 
-        id,
-        edit, 
-        editTableName, 
-        handleUpdatedTable, 
-        input, 
-        setTables, 
-        deleteGuest, 
-        setEdit, 
-        getUpdatedId, 
-        deleteTable,
-        guests
-        // handleSubmit 
-    } = props;
+  const {
+    tables,
+    table,
+    id,
+    edit,
+    editTableName,
+    handleUpdatedTable,
+    input,
+    setTables,
+    deleteGuest,
+    setEdit,
+    getUpdatedId,
+    deleteTable,
+    guests,
+    // handleSubmit
+  } = props;
 
-    const filteredGuests = guests?.filter(guest => guest.tableID === table._id);
+  const filteredGuests = guests?.filter((guest) => guest.tableID === table._id);
 
-    return(
-        <>
-            {edit.id === table._id ? (
-                <li key={id} className="home-cards fade-in render-tables" id="table-form-style table-card" style={props.edit.id === props.table._id ? {backgroundColor: `#F5F5F5`} : null}>
-                <div className="tbName">
-                    <form onSubmit={editTableName} className="mb-3">
-                        <input
-                        // ref={ref}
-                        type="text"
-                        className="shadow-none"
-                        name="name" 
-                        onChange={handleUpdatedTable}
-                        value={input}
-                        style={{background: "white"}}
-                        />
-                    </form>
-                </div>
-            
-                <Select table={table} tables={tables} setTables={setTables} guests={table.guestID}/>
-        
-                <div style={{marginBottom: '20px', marginTop: '20px', width: "100%"}}>
-                    {guests && filteredGuests.map(guest => {
-                        return <div key={guest._id} className="guest-del">
-                            <span>{guest.name}</span>
-                            <button 
-                                onClick={() => {deleteGuest(guest._id, table._id)}} 
-                                className=""
-                            >
-                                <i className="fas fa-times"></i>
-                            </button>
-                        </div>
-                    })}
-                </div>
-                {/* <div className="custom-dropdown dots-menu-edit">
+  return (
+    <>
+      {edit.id === table._id ? (
+        <li
+          key={id}
+          className="home-cards fade-in render-tables"
+          id="table-form-style table-card"
+          style={
+            props.edit.id === props.table._id && { backgroundColor: `#FFF` }
+          }
+        >
+          <div className="tbName">
+            <form onSubmit={editTableName}>
+              <input
+                // ref={ref}
+                type="text"
+                className="shadow-none"
+                name="name"
+                onChange={handleUpdatedTable}
+                value={input}
+                style={{ background: "#fff", border: "1px solid lightgrey" }}
+              />
+            </form>
+          </div>
+
+          <Select
+            table={table}
+            tables={tables}
+            setTables={setTables}
+            guests={table.guestID}
+          />
+
+          <div
+            style={{ marginBottom: "20px", marginTop: "20px", width: "100%" }}
+          >
+            {guests &&
+              filteredGuests.map((guest) => {
+                return (
+                  <div key={guest._id} className="guest-del">
+                    <span>{guest.name}</span>
+                    <IconButton
+                      onClick={() => {
+                        deleteGuest(guest._id, table._id);
+                      }}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </div>
+                );
+              })}
+          </div>
+          {/* <div className="custom-dropdown dots-menu-edit">
                     <Dropdown>
                         <Dropdown.Toggle as={CustomToggle} />
                         <Dropdown.Menu size="sm" title="">
@@ -72,17 +94,48 @@ const TableId = (props) => {
                         </Dropdown.Menu>
                     </Dropdown>
                 </div> */}
-                <div style={{ marginBottom: "20px", display: "flex", width: "100%", justifyContent: "space-evenly"}}>
-                    <button onClick={() => setEdit({id: null})} className="btn-style edit-table-btn" ><i className="fas fa-undo"></i></button>
-                    <button onClick={(e) => {editTableName(e)}} className="btn-style edit-table-btn"><i className="fas fa-check"></i></button>
-                </div>
-            </li>
-            ) : (
-                <Table table={table} edit={edit} setEdit={setEdit} getUpdatedId={getUpdatedId} deleteTable={deleteTable} guests={guests} />
-            )}
-        </>
-    )
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              alignItems: "center",
+              gap: "7px",
+            }}
+          >
+            <BlackButton
+              onClick={(e) => {
+                editTableName(e);
+              }}
+              text={"Enregistrer"}
+            />
+            <Button
+              onClick={() => setEdit({ id: null })}
+              style={{
+                backgroundColor: "none",
+                boxShadow: "none",
+                border: "none",
+                color: "#000",
+                textTransform: "unset",
+                fontSize: "1rem",
+              }}
+            >
+              Annuler
+            </Button>
+          </div>
+        </li>
+      ) : (
+        <Table
+          table={table}
+          edit={edit}
+          setEdit={setEdit}
+          getUpdatedId={getUpdatedId}
+          deleteTable={deleteTable}
+          guests={guests}
+        />
+      )}
+    </>
+  );
 };
-
 
 export default TableId;
--- src/components/AsyncSelect/AsyncSelect.js
+++ src/components/AsyncSelect/AsyncSelect.js
@@ -4,127 +4,133 @@ import axios from "axios";
 import "./AsyncSelect.css";
 
 const Select = ({ tables, table, guests, setTables }) => {
-    
-    const [loadingList, setloadingList] = useState(tables)
+  const [loadingList, setloadingList] = useState(tables);
 
-    const disableBtn = {
-        boxShadow: 'none',
-        backgroundColor: '#d6d6d6',
-        color: '#878787',
-        padding: '6px 19px',
-        borderRadius: '7px',
-        transition: '0.2s all ease-in-out'
-        // fontSize: ".75rem"
+  const disableBtn = {
+    boxShadow: "none",
+    backgroundColor: "#d6d6d6",
+    color: "#878787",
+    padding: "6px 19px",
+    borderRadius: "7px",
+    transition: "0.2s all ease-in-out",
+    // fontSize: ".75rem"
+  };
+
+  const enableBtn = {
+    boxShadow: "0 1px 3px 0 rgb(0 0 0 / 26%)",
+    backgroundColor: "darkgrey",
+    borderColor: "#caa5c1",
+    color: "#FFFFFF",
+    borderRadius: "5px",
+    padding: "6px 19px",
+    fontWeight: "400",
+    width: "fit-content",
+    marginTop: "10px",
+  };
+
+  const [selectedGuest, setSelectedGuest] = useState(null);
+  const [guest, setGuest] = useState(null);
+
+  const onSearchChange = (name) => {
+    if (name) {
+      setSelectedGuest(name);
     }
+  };
 
-    const enableBtn = {
-        boxShadow: "0 1px 3px 0 rgb(0 0 0 / 26%)",
-        backgroundColor: "darkgrey",
-        borderColor: "#caa5c1",
-        color: "#FFFFFF",
-        borderRadius: "5px",
-        padding: "6px 19px",
-        fontWeight: "400",
-        width: "fit-content",
-        marginTop: "10px"
-    }
-
-    
-    const [selectedGuest, setSelectedGuest] = useState(null);
-    const [guest, setGuest] = useState(null);
-
-    const onSearchChange = (name) => {
-        if(name){
-            setSelectedGuest(name)
+  const addGuest = async (selectedGuest, tableID) => {
+    selectedGuest = selectedGuest.value;
+    const updatedTables = [...loadingList].map((table) => {
+      if (table._id === tableID.id) {
+        table._id = selectedGuest;
+      }
+      return table;
+    });
+    await axios
+      .put(`/api/admin/guests/addtable/${selectedGuest}`, {
+        tableID: table._id,
+      })
+      .then((res) => {
+        if (res.data != null) {
+          setTimeout(() => {
+            setloadingList(updatedTables);
+            window.location.reload();
+          }, 1000);
         }
-    }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const addGuest = async (selectedGuest, tableID) => {
-        selectedGuest = selectedGuest.value
-        const updatedTables = [...loadingList].map((table) => {
-            if(table._id === tableID.id) {
-                table._id = selectedGuest
-            }
-            return table
-        })
-        await axios.put(`/api/admin/guests/addtable/${selectedGuest}`, {tableID: table._id})
-            .then((res) => {
-                if(res.data != null) {
-                    setTimeout(() => {
-                        setloadingList(updatedTables)
-                        window.location.reload()
-                    }, 1000);
-                }
-            })
-            .catch((err) => {
-                console.log(err)})
-    }
-
-
-    const loadOptions = (inputText, callback) => {
-        setTimeout(() => {
-            axios.get(`api/admin/guests/${inputText}`)
-            .then((res) => {
+  const loadOptions = (inputText, callback) => {
+    setTimeout(() => {
+      axios
+        .get(`api/admin/guests/${inputText}`)
+        .then((res) => {
+          // console.log(guest)
+          let array;
+          array = res.data;
+          const tempArray = [];
+          if (array) {
+            if (array.length) {
+              array.forEach((guest) => {
                 // console.log(guest)
-                let array;
-                array = res.data;
-                const tempArray = [];
-                if(array) {
-                    if(array.length){
-                        array.forEach((guest) => {
-                            // console.log(guest)
-                            setGuest(guest)
-                            tempArray.push({
-                                label: `${guest.name}`,
-                                value: guest._id,
-                                tableID: guest.tableID
-                            })
-                        })
-                    } else {
-                        tempArray.push({
-                            label: `${array.name}`,
-                            value: array._id,
-                            tableID: `${guest.tableID}`
-                        })
-                    }
-                }
-                callback(guests = tempArray)
-                // console.log(tempArray)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-        }, 1500);
-    }
+                setGuest(guest);
+                tempArray.push({
+                  label: `${guest.name}`,
+                  value: guest._id,
+                  tableID: guest.tableID,
+                });
+              });
+            } else {
+              tempArray.push({
+                label: `${array.name}`,
+                value: array._id,
+                tableID: `${guest.tableID}`,
+              });
+            }
+          }
+          callback((guests = tempArray));
+          // console.log(tempArray)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, 1500);
+  };
 
-    // const loadOptions = async (callback) => {
-    //     const token = localStorage.getItem("token");
-    //     const config = {
-    //         headers: { Authorization: 'Bearer '+ token }
-    //         };
-    //     const data = await fetch(`api/admin/guests/`, config)
-    //     const formatted = await data.json()
-    //     console.log(formatted)
-    //     callback(formatted.map(guest => ({ label: guest.name, value: guest._id, avatar: guest.media })))
-    // }
-    return(
-        <div className="select-style" >
-            <AsyncSelect 
-            value={selectedGuest}
-            loadOptions={loadOptions}
-            onChange={e => onSearchChange(e)}
-            defaultOptions={true}
-            // inputValue={true}
-            isOptionDisabled={(option) => option.tableID != null }
-            />
-            <button 
-                className="add-btn" 
-                disabled={!selectedGuest} 
-                style={!selectedGuest ? disableBtn : enableBtn} 
-                onClick={() => {addGuest(selectedGuest, table)}}
-            >Sélectionner</button>
-        </div>
-    )
-}
+  // const loadOptions = async (callback) => {
+  //     const token = localStorage.getItem("token");
+  //     const config = {
+  //         headers: { Authorization: 'Bearer '+ token }
+  //         };
+  //     const data = await fetch(`api/admin/guests/`, config)
+  //     const formatted = await data.json()
+  //     console.log(formatted)
+  //     callback(formatted.map(guest => ({ label: guest.name, value: guest._id, avatar: guest.media })))
+  // }
+  return (
+    <div className="select-style">
+      <AsyncSelect
+        value={selectedGuest}
+        loadOptions={loadOptions}
+        onChange={(e) => onSearchChange(e)}
+        defaultOptions={true}
+        // inputValue={true}
+        isOptionDisabled={(option) => option.tableID != null}
+      />
+      <button
+        className="add-btn"
+        disabled={!selectedGuest}
+        style={!selectedGuest ? disableBtn : enableBtn}
+        onClick={() => {
+          addGuest(selectedGuest, table);
+        }}
+      >
+        Ajouter
+      </button>
+    </div>
+  );
+};
 
 export default Select;
