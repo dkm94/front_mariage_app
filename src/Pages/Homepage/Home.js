42212503917e3bf75611a64a3ad7f185174d6150
dkm94 <diane.mpacko@gmail.com>
2023-06-29 13:43:16 +0200

    update home page section-1

--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -1,63 +1,79 @@
-import React from 'react';
+import React from "react";
 import { withRouter, Link } from "react-router-dom";
 import flowers from "../../img/bouquet.png";
-import './Home.css';
-import "../../components/LargeButton/LargeButton.css"
-import { Container, Row } from 'react-bootstrap';
+import "./Home.css";
+import "../../components/LargeButton/LargeButton.css";
+import { Container, Row } from "react-bootstrap";
 import Card from "./Card";
 import dashboardCards from "./data";
+import { Button } from "@mui/material";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <div className="section-1">
-                <div className="home___position">
-                    <div className="home___position-style">
-                        <div className="home-desc">
-                            <h1>Simplifiez l'organisation de votre mariage</h1>
-                            <div className="home__picture" id="hidden-picture">
-                                <img src={flowers} alt="home page illustration flowers" />
-                            </div>
-                            <p>
-                                <span>Consultez et gérez facilement les étapes clés de votre mariage en quelques clics.</span>
-                                <span>My Wedding, l'outil indispensable pour ne rien oublier des préparatifs de l'un des plus beaux évènements de votre vie.</span>
-                            </p>
-                        </div>
-                        <div className="home___register-btn">
-                            <Link to={"/register"} className="home-link btn-style">
-                            Inscrivez-vous dès maintenant
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="home__picture" id="show-home-picture">
-                    <img src={flowers} alt="home page illustration flowers" />
-                </div>
+  return (
+    <div className="home">
+      <div className="section-1">
+        <div className="home___position">
+          <div className="home___position-style">
+            <div className="home-desc">
+              <h1>Simplifiez l'organisation de votre mariage</h1>
+              <div className="home__picture" id="hidden-picture">
+                <img src={flowers} alt="home page illustration flowers" />
+              </div>
+              <p>
+                <span>
+                  Consultez et gérez facilement les étapes clés de votre mariage
+                  en quelques clics.
+                </span>
+                <span>
+                  My Wedding, l'outil indispensable pour ne rien oublier des
+                  préparatifs de l'un des plus beaux évènements de votre vie.
+                </span>
+              </p>
             </div>
-            <div className="section-2">
-                <div className="section-2___titre">
-                    <h2>Comment ça fonctionne</h2>
-                </div>
-                <div className="section-2___sous-titre">
-                    <span>Votre tableau de bord vous apporte une vue d'ensemble des éléments ajoutés aux différentes catégories.</span>
-                </div>
-                <Container>
-                    <Row>
-                        {dashboardCards.map((card, i) => {
-                            return(
-                                <Card
-                                index={i}
-                                icon={card.icon}
-                                title={card.title}
-                                description={card.description}
-                            />
-                            )
-                        })}
-                    </Row>
-                </Container>
+            <div className="home___register-btn">
+              <Button variant="contained">
+                <Link
+                  to={"/register"}
+                  className="home-link"
+                  style={{ fontSize: "1.2rem" }}
+                >
+                  Inscrivez-vous
+                </Link>
+              </Button>
             </div>
+          </div>
         </div>
-    )
-}
+        <div className="home__picture" id="show-home-picture">
+          <img src={flowers} alt="home page illustration flowers" />
+        </div>
+      </div>
+      <div className="section-2">
+        <div className="section-2___titre">
+          <h2>Comment ça fonctionne</h2>
+        </div>
+        <div className="section-2___sous-titre">
+          <span>
+            Votre tableau de bord vous apporte une vue d'ensemble des éléments
+            ajoutés aux différentes catégories.
+          </span>
+        </div>
+        <Container>
+          <Row>
+            {dashboardCards.map((card, i) => {
+              return (
+                <Card
+                  index={i}
+                  icon={card.icon}
+                  title={card.title}
+                  description={card.description}
+                />
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
 export default withRouter(Home);
--- src/components/Header/Navigation/Log_out.js
+++ src/components/Header/Navigation/Log_out.js
@@ -1,25 +1,34 @@
- import React from 'react';
- import { Link, withRouter } from 'react-router-dom';
- import "../Header.css";
- import Logo from "../../../img/desktop-logo.png";
- import MobileLogo from "../../../img/mobile-logo.png";
- 
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import "../Header.css";
+import Logo from "../../../img/logo-2023-no-shadow.png";
+import { Button } from "@mui/material";
+
 const Logout = () => {
+  const path = window.location.pathname;
 
-    const path = window.location.pathname;
-
-    return (
-    <div className="header header-style" style={path === "/" ? {backgroundColor: "#e8e8e4"} : {backgroundColor: "#FFF"}}>
-        <div className="navigation">
-            <div id="brand">
-                <Link to={{ pathname: "/"}}><img alt="logo" src={Logo} id="logo" /></Link>
-                <img alt="mobile-logo" src={MobileLogo} id="mobile-logo" />
-            </div>
-            <ul className="navbar-menu">
-                <li className="li-style" id="login"><Link to={"/login"}>Connexion</Link></li>
-            </ul>
+  return (
+    <div
+      className="header header-style"
+      style={
+        path === "/"
+          ? { backgroundColor: "#e8e8e4" }
+          : { backgroundColor: "#FFF" }
+      }
+    >
+      <div className="navigation">
+        <div id="brand">
+          <Link to={{ pathname: "/" }}>
+            <img alt="logo" src={Logo} id="logo" />
+          </Link>
         </div>
+        <ul className="navbar-menu">
+          <Button variant="outlined" className="li-style" id="login">
+            <Link to={"/login"}>Connexion</Link>
+          </Button>
+        </ul>
+      </div>
     </div>
-    )
-}
-export default withRouter(Logout)
+  );
+};
+export default withRouter(Logout);
