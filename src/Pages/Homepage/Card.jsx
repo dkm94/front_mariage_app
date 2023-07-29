4a0d9aa9e2034c0d1dbf3396ef1d68ee4a0f8d56
dkm94 <diane.mpacko@gmail.com>
2023-07-29 23:53:34 +0200

    + update banner image + update 2nd home section + update home cards section (grid) + bugfix register button

--- src/Pages/Homepage/Card.jsx
+++ src/Pages/Homepage/Card.jsx
@@ -4,8 +4,7 @@ import { Col, Container } from "react-bootstrap";
 
 const Card = ({ index, icon, title, description }) => {
   return (
-    <Col xs={12} sm={6} md={4} mt={4} style={{ marginTop: "2rem" }} key={index}>
-      <Container className="home-cards" id="intro-card" style={{ gap: "7px" }}>
+      <div key={index} className="home-cards" id="intro-card">
         <div className="home-cards__img">
           <img src={icon} alt="icône invités" />
         </div>
@@ -15,8 +14,7 @@ const Card = ({ index, icon, title, description }) => {
         <div>
           <span>{description}</span>
         </div>
-      </Container>
-    </Col>
+      </div>
   );
 };
 
