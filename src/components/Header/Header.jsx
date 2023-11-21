1e106e579dfbd6194d4475c9e692054996dd64a8
dkm94 <diane.mpacko@gmail.com>
2023-11-21 17:31:01 +0100

    + updated Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
-import { withRouter } from "react-router-dom";
 import "./Header.css";
+
+import React from 'react';
+import { withRouter } from "react-router-dom";
+
 import Login from "../Navigation/Log_in";
 import Logout from "../Navigation/Log_out";
 
 const Header = ({ userInfos: { invitationID } }) => {
 
     const token = localStorage.getItem("token");
-        let navigation;
-        if(token === null) {
-            navigation =  <Logout />
-        } else 
-            navigation =  <Login invitationId={invitationID}/>
+    let navigation;
+    if(token === null) {
+        navigation =  <Logout />
+    } else 
+        navigation =  <Login invitationId={invitationID}/>
+        
     return (
         <div className="header">
             {navigation}
