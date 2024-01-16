c772a2798b93a1ad42d08d951bf857fa6e58f902
dkm94 <diane.mpacko@gmail.com>
2024-01-16 12:13:50 +0100

    + added layout to guests page

--- src/Pages/Invités/Invités.tsx
+++ src/Pages/Invités/Invités.tsx
@@ -1,24 +1,19 @@
 import "./Invités.css";
 import "../../components/Invités(affichage)/by_guests/guests.css";
 
-import React, { useState, useEffect, useContext, ChangeEvent } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 
 import { Container, Row, Col } from "react-bootstrap";
-import Grow from "@mui/material/Grow";
 
 import { GuestType } from "../../../types";
 import { getGuests } from "../../services/guests/guestRequests";
-import { ScrollButtonContext } from "../../App";
 
 import AddForm from "../../components/Invités(affichage)/by_guests/Components/Form/AddGuest";
 import GuestList from "../../components/Invités(affichage)/by_guests/Components/Guests/Guests";
 import SearchBar from "../../components/Invités(affichage)/by_guests/Components/SearchBar/SearchBar";
-import ScreenLoader from "../../components/Loader/Screen/ScreenLoader.jsx";
 import { useFetch } from "../../hooks";
-import PageTitle from "../../components/LayoutPage/PageTitle/PageTitle";
-import PageBanner from "../../components/LayoutPage/PageBanner/PageBanner";
+import ContentLayout from "../../components/LayoutPage/ContentLayout/ContentLayout";
 
 type NewUser = string;
 
@@ -36,7 +31,6 @@ interface ByGuestsProps {
 
 const Byguests = (props: ByGuestsProps) => {
   const { firstPerson, secondPerson, mariageID } = props.userInfos;
-  const scrollBtn = useContext(ScrollButtonContext);
 
   const [newUser, setNewUser] = useState<NewUser>("");
 
@@ -127,67 +121,50 @@ const Byguests = (props: ByGuestsProps) => {
   };
 
   return (
-    <>
-      {loading ? (
-        <ScreenLoader />
-      ) : (
-        <div className="byguests page-component">
-          <div className="guest-container">
-            {scrollBtn}
-
-            <PageTitle loading={loading} title="Souhaitez-vous ajouter de nouveaux invités ?" />
-
-            <PageBanner loading={loading} src={"guests"}  />
-
-            <Grow in={!loading} timeout={2000}>
-              <Container style={{ padding: "2rem 50px" }} fluid>
-                <Row>
-                  <Col xs={12} sm={10} md={6} className="guest-form">
-                    <AddForm newUser={newUser} setNewUser={setNewUser} addGuest={addGuest} />
-                  </Col>
-                  <Col xs={12} sm={10} md={6} className="searchbar">
-                    <SearchBar
-                      className="search__input"
-                      type="text"
-                      placeholder="Rechercher un invité"
-                      name="searchbar"
-                      value={searchValue}
-                      onChange={handleSearch}
-                    />
-                  </Col>
-                </Row>
-              </Container>
-            </Grow>
-
-            <Grow in={!loading} timeout={2000}>
-              <div className="guests___list">
-                <div className="byguests___block">
-                  <GuestList
-                    guests={guests}
-                    setGuests={setGuests}
-                    deleteGuest={deleteGuest}
-                    updateGuest={editGuest}
-                    editPicture={editPicture}
-                    seteditPicture={seteditPicture}
-                    upload={uploadPicture}
-                    handleFile={handleFile}
-                    searchValue={searchValue}
-                    mariageID={mariageID}
-                    appear={appear}
-                    firstPerson={firstPerson}
-                    secondPerson={secondPerson}
-                    isOpen={isOpen}
-                    setisOpen={setisOpen}
-                    errorMessage={errorMessage}
-                    error={error}
-                  />
-                </div>
-              </div>
-            </Grow>
+    <ContentLayout loading={loading} title={"Souhaitez-vous ajouter de nouveaux invités ?"} src={"guests"} >
+      <>
+        <Container style={{ padding: "2rem 50px" }} fluid>
+          <Row>
+            <Col xs={12} sm={10} md={6} className="guest-form">
+              <AddForm newUser={newUser} setNewUser={setNewUser} addGuest={addGuest} />
+            </Col>
+            <Col xs={12} sm={10} md={6} className="searchbar">
+              <SearchBar
+                className="search__input"
+                type="text"
+                placeholder="Rechercher un invité"
+                name="searchbar"
+                value={searchValue}
+                onChange={handleSearch}
+              />
+            </Col>
+          </Row>
+        </Container>
+        <div className="guests___list">
+          <div className="byguests___block">
+            <GuestList
+              guests={guests}
+              setGuests={setGuests}
+              deleteGuest={deleteGuest}
+              updateGuest={editGuest}
+              editPicture={editPicture}
+              seteditPicture={seteditPicture}
+              upload={uploadPicture}
+              handleFile={handleFile}
+              searchValue={searchValue}
+              mariageID={mariageID}
+              appear={appear}
+              firstPerson={firstPerson}
+              secondPerson={secondPerson}
+              isOpen={isOpen}
+              setisOpen={setisOpen}
+              errorMessage={errorMessage}
+              error={error}
+            />
           </div>
         </div>
-      )}
-    </>
+      </>
+    </ContentLayout>
   );
 };
 
