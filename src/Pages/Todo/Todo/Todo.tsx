cf1503b1035d63f71c45a7024ccdb7d6561037e1
dkm94 <diane.mpacko@gmail.com>
2024-02-01 20:00:29 +0100

    + refacto css Todos component

--- src/Pages/Todo/Todo/Todo.tsx
+++ src/Pages/Todo/Todo/Todo.tsx
@@ -1,6 +1,8 @@
-import "./List.css";
+import "./Todo.css";
 
-import React, { useState, useRef } from "react";
+import React, { useState, Dispatch, SetStateAction } from "react";
+import { useHistory, useParams } from "react-router";
+import { History } from "history";
 
 import { IconButton } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
@@ -8,11 +10,10 @@ import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 import CustomIconButton from "../../../components/Buttons/SmallIconButton/IconButton";
-
 import UpdateForm from "../Update/Form";
+
 import { updateTodosStatus, deleteTodo } from '../../../services';
 import { TaskType } from "../../../../types";
-import { useHistory, useParams } from "react-router";
 import { useCurrentUser } from "../../../ctx/userCtx";
 
 type EditType = {
@@ -20,35 +21,41 @@ type EditType = {
   text?: string;
 }
 
-const Todos = ({
-  todos,
-  setTodos,
-  searchValue,
-  setSearchValue,
-  obj,
-  i,
-  isOpen,
-  setisOpen,
-  setMessage,
-  setMessageType,
-  setTodo
-}) => {
-  const history = useHistory();
+interface TodosProps {
+  todos: TaskType[];
+  setTodos: Dispatch<SetStateAction<TaskType[]>>;
+  todo: TaskType;
+  setMessage: Dispatch<SetStateAction<string | undefined>>;
+  setMessageType: Dispatch<SetStateAction<"error" | "success" | undefined>>;
+  setTodo: Dispatch<SetStateAction<string | null>>;
+}
+
+const Todo = (props: TodosProps) => {
+  const { 
+    todos, 
+    setTodos, 
+    todo, 
+    setMessage, 
+    setMessageType, 
+    setTodo } = props;
+
+  const history: History = useHistory();
   const { mariageID } = useCurrentUser();
   const { id: todoId } = useParams<{id: string}>();
 
   const [edit, setEdit] = useState<EditType | null>(null);
-  const [input, setInput] = useState("");
-  const inputRef = useRef(null);
+  const [input, setInput] = useState<string>("");
 
-  const getUpdatedId = (objId: string, objText: string) => {
+  const getUpdatedId = (objId: string, objText: string): void => {
     setEdit({
       id: objId,
     });
     setInput(objText);
   };
 
-  const toggleCompleted = async (task) => {
+  const toggleCompleted = async (task: TaskType): Promise<void> => {
+    setTodo(task._id)
+
     const updatedList = [...todos].map((todo) => {
       if (todo._id === task._id) {
         todo.isCompleted = !task.isCompleted;
@@ -57,21 +64,33 @@ const Todos = ({
     });
 
     const response = await updateTodosStatus({ isCompleted: task.isCompleted, id: task._id })
-    const { success, message } = response;
+    const { success } = response;
 
     if(!success) {
       setMessageType("error");
-      setMessage(message);
+      setMessage("Oups, une erreur est survenue lors de la modification de l'état de la tâche");
+
+      setTimeout(() => {
+        setMessage(undefined);
+        setMessageType(undefined);
+        setTodo(null);
+      }, 2000);
       return;
     }
 
+    setTodos(updatedList);
+    setMessageType("success");
+    setMessage("Modifications enregistrées");
+
     setTimeout(() => {
-      setTodos(updatedList);
-    }, 500);
+      setMessageType(undefined);
+      setMessage(undefined);
+      setTodo(null);
+    }, 2000);
   };
 
-  const deleteTodoFn = async (id: string) => {
-    setTodo({ _id: id })
+  const deleteTodoFn = async (id: string): Promise<void> => {
+    setTodo(id);
 
     const response = await deleteTodo({ id })
     const { success, message } = response;
@@ -79,34 +98,52 @@ const Todos = ({
     if(!success) {
       setMessageType("error");
       setMessage(message);
+
+      setTimeout(() => {
+        setMessage(undefined);
+        setMessageType(undefined);
+        setTodo(null);
+      }, 2000);
       return;
     }
 
     setMessageType("success");
     setMessage(message);
     setTodos(todos.filter((todo: TaskType) => todo._id !== id));
+
+    setTimeout(() => {
+      setMessage(undefined);
+      setMessageType(undefined);
+      setTodo(null);
+    }, 2000);
   };
 
+
+  const handleEditTodo = (todo: TaskType): void => {
+    getUpdatedId(todo._id, todo.text);
+
+    const currentPosition: number = window.scrollY;
+    history.replace(`/mariage/${mariageID}/taches/edit/${todoId}`, { currentPosition })
+  };
 
   return (
     <Grid2
       component="div" // Add the component prop with value "div"
       xs={12}
-      key={obj._id}
+      key={todo._id}
       className={
-        obj.isCompleted
+        todo.isCompleted
           ? "tasks-list__li__done fade-in"
           : "tasks-list__li fade-in"
       }
-      style={edit === obj._id ? { backgroundColor: `#F5F5F5` } : undefined} // Change null to undefined
+      style={edit === todo._id ? { backgroundColor: `#F5F5F5` } : undefined} // Change null to undefined
     >
-      {edit?.id === obj._id ? (
+      {edit?.id === todo._id ? (
         <UpdateForm
           edit={edit}
           setEdit={setEdit}
           input={input}
           setInput={setInput}
-          inputRef={inputRef}
           setTodos={setTodos}
           todos={todos}
           setMessage={setMessage}
@@ -123,35 +160,30 @@ const Todos = ({
           flexWrap={"inherit"}
         >
           <Grid2 display={"flex"} alignItems={"center"} width={"100%"}>
-            {obj.isCompleted ? (
-              <IconButton onClick={() => toggleCompleted(obj)}>
+            {todo.isCompleted ? (
+              <IconButton onClick={() => toggleCompleted(todo)}>
                 <CheckCircleIcon fontSize="small" />
               </IconButton>
             ) : (
-              <IconButton onClick={() => toggleCompleted(obj)}>
+              <IconButton onClick={() => toggleCompleted(todo)}>
                 <RadioButtonUncheckedIcon />
               </IconButton>
             )}
-            <span>{obj.text}</span>
+            <span>{todo.text}</span>
           </Grid2>
           <Grid2 className="todolist___checkbox_span">
             
             <CustomIconButton
             type="submit"
             buttonType='edit' 
-            obj={obj} 
-            onClick={() => {
-              getUpdatedId(obj._id, obj.text);
-
-              const currentPosition: number = window.scrollY;
-              history.replace(`/mariage/${mariageID}/taches/edit/${todoId}`, { currentPosition })
-            }} 
+            obj={todo} 
+            onClick={() => handleEditTodo(todo)} 
             />
 
             <CustomIconButton 
             type="submit"
             buttonType="delete"
-            onClick={() => deleteTodoFn(obj._id)} 
+            onClick={() => deleteTodoFn(todo._id)} 
             />
 
           </Grid2>
@@ -161,4 +193,4 @@ const Todos = ({
   );
 };
 
-export default Todos;
+export default Todo;
---
+++ src/Pages/Todo/Update/Form.tsx
@@ -0,0 +1,126 @@
+import "./Form.css";
+
+import React, { ChangeEvent, Dispatch, FormEvent, RefObject, SetStateAction, useEffect, useRef } from "react";
+import { useHistory } from 'react-router';
+
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { TextField } from "@mui/material";
+
+import { updateTodo } from "../../../services";
+import { TaskType } from "../../../../types";
+
+import CustomIconButton from '../../../components/Buttons/SmallIconButton/IconButton';
+
+type EditType = {
+  id?: string;
+  text?: string;
+}
+
+interface UpdateTaskFormProps {
+  edit: EditType | null;
+  input: string;
+  setEdit: Dispatch<SetStateAction<EditType | null>>;
+  setInput: Dispatch<SetStateAction<string>>;
+  setTodos: Dispatch<SetStateAction<TaskType[]>>;
+  todos: TaskType[];
+  setMessage:Dispatch<SetStateAction<string | undefined>>;
+  setMessageType: Dispatch<SetStateAction<"error" | "success" | undefined>>;
+  mariageID: string;
+}
+
+const UpdateTask = (props: UpdateTaskFormProps) => {
+  const { 
+    edit, 
+    input, 
+    setEdit, 
+    setInput, 
+    setTodos, 
+    todos, 
+    setMessage, 
+    setMessageType, 
+    mariageID } = props;
+
+  const history = useHistory();
+  const inputRef:RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
+  
+  useEffect(() => {
+    inputRef?.current?.focus();
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setInput(e.target.value);
+  };
+
+  const editTodo = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
+
+    const response = await updateTodo({ text: input, id: edit?.id })
+    const { success, message } = response;
+    
+    if(!success) {
+      setMessageType("error");
+      setMessage(message);
+      return;
+    }
+
+    const todosCopy = [...todos]
+    const selectedTodo = todosCopy.find((t) => t._id === edit?.id);
+    if(selectedTodo){
+      selectedTodo.text = input;
+    }
+    setTimeout(() => {
+      setTodos([...todosCopy]);
+      setEdit(null);
+      setInput("");
+    }, 1000);
+
+    const currentPosition: number = window.scrollY;
+    history.replace(`/mariage/${mariageID}/taches`, { currentPosition })
+  };
+
+  const handleCancel = () => {
+    setEdit(null);
+    setInput("");
+
+    const currentPosition: number = window.scrollY;
+    history.replace(`/mariage/${mariageID}/taches`, { currentPosition })
+  }
+
+  return (
+    <Grid2
+      container
+      display={"flex"}
+      flexDirection={"row"}
+      p={"1rem 3rem"}
+      width={"100%"}
+    >
+      <form onSubmit={editTodo} className="todo-form">
+        <Grid2 width={"100%"}>
+          <TextField
+            size="small"
+            style={{
+              width: "100%",
+              backgroundColor: "#fff",
+            }}
+            type="text"
+            name="text"
+            onChange={handleChange}
+            value={input}
+            ref={inputRef}
+          />
+        </Grid2>
+
+        <Grid2 display={"flex"} gap={"7px"}>
+          <CustomIconButton type="submit" buttonType="save"/>
+          <CustomIconButton
+          type="reset" 
+          buttonType="cancel" 
+          onClick={handleCancel}
+          />
+        </Grid2>
+      </form>
+    </Grid2>
+  );
+};
+
+export default UpdateTask;
