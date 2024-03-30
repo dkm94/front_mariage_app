435235c0eede03ed09839fd60accae119c5ec8d3
dkm94 <diane.mpacko@gmail.com>
2024-03-30 04:28:13 +0100

    + updated homepage with form section + responsive ok

--- src/Pages/Homepage/components/Header/Header.tsx
+++ src/Pages/Homepage/components/Header/Header.tsx
@@ -5,29 +5,31 @@ import img from "../../../../img/img-home.png";
 
 const HomeHeader = () => {
   return (
-    <section id="home-header">
-      <div id="header-text">
-        <div className="big-text">
-          <span>Prêts ?</span>
-          <span>Feu...</span>
-          <span>Mariés !</span>
+    <section id="home-header-wrapper">
+        <div id="home-header">
+          <div id="header-text">
+            <div className="big-text">
+              <span>Prêts ?</span>
+              <span>Feu...</span>
+              <span>Mariés !</span>
+            </div>
+            <div className="small-text">
+              <span>Simplifiez l'organisation de votre mariage</span>
+              <span>Consultez et gérez facilement les étapes clés de votre mariage en quelques clics.</span>
+              <span>My Wedding, l'outil indispensable pour ne rien oublier des préparatifs de l'un des plus beaux évènements de votre vie.</span>
+            </div>
+          </div>
+          <div id="header-image">
+            <div className="img-container">
+              <img id="home-image" src={img} alt="Wedding Couple" />
+            </div>
+          </div>
+          <div id="small-text-sm">
+            <span>Simplifiez l'organisation de votre mariage</span>
+            <span>Consultez et gérez facilement les étapes clés de votre mariage en quelques clics.</span>
+            <span>My Wedding, l'outil indispensable pour ne rien oublier des préparatifs de l'un des plus beaux évènements de votre vie.</span>
+          </div>
         </div>
-        <div className="small-text">
-          <span>Simplifiez l'organisation de votre mariage</span>
-          <span>Consultez et gérez facilement les étapes clés de votre mariage en quelques clics.</span>
-          <span>My Wedding, l'outil indispensable pour ne rien oublier des préparatifs de l'un des plus beaux évènements de votre vie.</span>
-        </div>
-      </div>
-      <div id="header-image">
-        <div className="img-container">
-          <img id="home-image" src={img} alt="Wedding Couple" />
-        </div>
-      </div>
-      <div id="small-text-sm">
-        <span>Simplifiez l'organisation de votre mariage</span>
-        <span>Consultez et gérez facilement les étapes clés de votre mariage en quelques clics.</span>
-        <span>My Wedding, l'outil indispensable pour ne rien oublier des préparatifs de l'un des plus beaux évènements de votre vie.</span>
-      </div>
     </section>
   )
 }
--- src/Pages/Homepage/components/Signup/Signup.tsx
+++ src/Pages/Homepage/components/Signup/Signup.tsx
@@ -1,10 +1,19 @@
 import "./style.css";
-
 import React from 'react'
+
+import { CustomButton } from "../../../../components/Buttons";
 
 const HomeSignup = () => {
   return (
-    <div>HomeSignup</div>
+    <section id="home-signup-wrapper">
+      <div id="signup-box">
+        <span>Inscrivez-vous dès maintenant !</span>
+        <form id="signup-box-form">
+          <input type="text" autoComplete="off" name="email" value={""} />
+          <CustomButton text="Créer un compte" />
+        </form>
+      </div>
+    </section>
   )
 }
 
