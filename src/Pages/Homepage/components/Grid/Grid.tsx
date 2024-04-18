45c78a767180e13936b1e99a63963c66241d4da8
dkm94 <diane.mpacko@gmail.com>
2024-04-18 02:36:16 +0200

    + updated homepage

--- src/Pages/Homepage/components/Grid/Grid.tsx
+++ src/Pages/Homepage/components/Grid/Grid.tsx
@@ -1,32 +1,48 @@
 import "./style.css";
 
 import React from 'react'
-import { Container } from "@mui/material";
 
 import dashboardCards from "../../data";
 import Card from "../../Card";
 
+import img from "../../../../img/home/mobile-browser.svg";
 
 const HomeGrid = () => {
   return (
     <section className='presentation-section'>
-        <Container className='container'>
-          <div className="section-2___titre">
+        <div className='presentation-wrapper'>
+          <div className="description-cards">
             <h2>Comment ça fonctionne</h2>
+            <div className="title">
+              <span>Triez vos listes d'amis, dépenses et bien d'autres</span>
+            </div>
           </div>
-          <div className='cards-container'>
-              {dashboardCards.map((card, i) => {
-                return (
-                  <Card
-                    index={i}
-                    icon={card.icon}
-                    title={card.title}
-                    description={card.description}
-                  />
-                );
-              })}
+          <div id="presentation-details">
+            <div id="details-img">
+              <img src={img} alt="mobile view" />
+            </div>
+            <div id="details-description">
+              <div className="description-cards">
+                <h2>Comment ça fonctionne</h2>
+                <div className="title">
+                  <span>Triez vos listes d'amis, dépenses et bien d'autres</span>
+                </div>
+              </div>
+              <div className='cards-container'>
+                {dashboardCards.map((card, i) => {
+                  return (
+                    <Card
+                      index={i}
+                      icon={card.icon}
+                      title={card.title}
+                      description={card.description}
+                    />
+                  );
+                })}
+              </div>
+            </div>
           </div>
-        </Container>
+        </div>
       </section>
   )
 }
