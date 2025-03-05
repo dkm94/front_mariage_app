e0befd126e42241b63066c7f115f7872fa8f38e3
dkm94 <diane.mpacko@gmail.com>
2025-03-05 15:18:51 +0100

    updated QASM-TC-8 test case

--- cypress/e2e/auth/login.cy.ts
+++ cypress/e2e/auth/login.cy.ts
@@ -14,11 +14,14 @@ describe('Login Form', () => {
         cy.contains('Se connecter').should('not.exist');
     });
 
-    it('should switch from register to login form when the bottom link is clicked', () => { 
+    it('should switch from register to login form when the bottom link is clicked and match the URL "/login"', () => { 
         // BEWARE !! This is not a link but a button !
         cy.openRegisterForm();
+        cy.url().should('include', '/register');
+
         const loginBtn = cy.get('.register__signup > button').contains('Connectez-vous');
         loginBtn.should('be.visible').click();
         cy.contains('Se connecter').should('be.visible');
+        cy.url().should('include', '/login');
     });
 });
