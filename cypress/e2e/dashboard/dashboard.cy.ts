3f316e5acfa5fff36fb1909dd06281fdc7f5eaa4
dkm94 <diane.mpacko@gmail.com>
2025-04-02 05:18:01 +0200

    + new test /dashboard

--- cypress/e2e/dashboard/dashboard.cy.ts
+++ cypress/e2e/dashboard/dashboard.cy.ts
@@ -12,13 +12,45 @@ describe('Dashboard', () => {
     });
 
     it('should have "Voir détails" buttons that redirects to main features', () => {
+        const sidebar = cy.get('.custom-sidebar-menu');
+        sidebar.should('be.visible');
+        sidebar.should('contain', 'Tableau de bord');
         
+        // INVITES redirects to "/invites"
+        const invitesBtn = cy.get(':nth-child(1) > #dashboard-cards > .dashbord-view-details > a > .MuiButtonBase-root');
+        invitesBtn.click();
+        cy.url().should('include', '/invites');
 
-        // cy.get('[data-cy="dashboard-cards"]').within(() => {
-        //     cy.get('a').each(($el) => {
-        //         const href = $el.prop('href');
-        //         cy.wrap($el).should('have.attr', 'href').and('include', href);
-        //     });
-        // });
+        const dashboardHref = cy.get('.custom-sidebar-menu > [href="/mariage/660832cdb68f18004dd08894/tableau-de-bord"]');
+        dashboardHref.should("be.visible").click();
+
+        // TABLES redirects to "/tables"
+        const tablesBtn = cy.get(':nth-child(2) > #dashboard-cards > .dashbord-view-details > a > .MuiButtonBase-root');
+        tablesBtn.click();
+        cy.url().should('include', '/tables');
+
+        dashboardHref.click();
+
+        // RECEPTION redirects to "/carte"
+        const receptionBtn = cy.get(':nth-child(3) > #dashboard-cards > .dashbord-view-details > a > .MuiButtonBase-root');
+        receptionBtn.click();
+        cy.url().should("include", "/carte");
+
+        dashboardHref.click();
+
+        // TASKS redirects to "/taches"
+        const tasksBtn = cy.get(':nth-child(4) > #dashboard-cards > .dashbord-view-details > a > .MuiButtonBase-root');
+        tasksBtn.click();
+        cy.url().should("include", "/taches");
+
+        dashboardHref.click();
+
+        // EXPENSES redirects to "/budget"
+        const expensesBtn = cy.get(':nth-child(5) > #dashboard-cards > .dashbord-view-details > a > .MuiButtonBase-root');
+        expensesBtn.click();
+        cy.url().should("include", "/budget");
+
+        dashboardHref.click();
+        cy.url().should("include", "/tableau-de-bord");
     });
 });
